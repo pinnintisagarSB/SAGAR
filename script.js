357fb04b8ca29cf10417479d0c572457bb21c1b3
pinnintisagarSB <72218665+pinnintisagarSB@users.noreply.github.com>
2021-05-03 13:16:16 +0530

    Update 2.0
    Fixed menu-bar working

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
-let a=1;
-ico=document.getElementById('icon');
-ico.addEventListener('click',function(){
-    if(a==1){
+let a = 1;
+ico = document.getElementById('icon');
+ico.addEventListener('click', function () {
+    if (a == 1) {
         disablescroll();
-        console.log("adding class");
-        let mob=document.getElementsByClassName('mobile-nav');
+        console.log("Open");
+        let mob = document.getElementsByClassName('mobile-nav');
         mob[0].classList.add("clk");
-        return a=0;
+        var l= document.getElementsByClassName("logo");
+        l[0].classList.add("cl");
+        return a = 0;
     }
-    else{
+    else {
         enablescroll();
-        console.log("Deleteing class");
-        let mob=document.getElementsByClassName('mobile-nav');
+        console.log("Close");
+        let mob = document.getElementsByClassName('mobile-nav');
         mob[0].classList.remove("clk");
-        return a=1;
+        var l= document.getElementsByClassName("logo");
+        l[0].classList.remove("cl");
+        return a = 1;
     }
 });
-function disablescroll(){
-    scrollTop=document.documentElement.scrollTop;
-    scrollLeft=document.documentElement.scrollLeft;
-    window.onscroll=function(){
-        window.scrollTo(scrollTop,scrollLeft);
+function disablescroll() {
+    scrollTop = document.documentElement.scrollTop;
+    scrollLeft = document.documentElement.scrollLeft;
+    window.onscroll = function () {
+        window.scrollTo(scrollTop, scrollLeft);
     };
 };
-function enablescroll(){
-    window.onscroll =function(){};
+function enablescroll() {
+    window.onscroll = function () { };
 };
