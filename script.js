bb4b73d62742e8b090fba195ce8bcae8acf0a7bd
pinnintisagarSB <72218665+pinnintisagarSB@users.noreply.github.com>
2021-05-03 11:34:23 +0530

    Update 1.2
    menu-bar working

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+let a=1;
+ico=document.getElementById('icon');
+ico.addEventListener('click',function(){
+    if(a==1){
+        console.log("adding class");
+        let mob=document.getElementsByClassName('mobile-nav');
+        mob[0].classList.add("clk");
+        return a=0;
+    }
+    else{
+        console.log("Deleteing class");
+        let mob=document.getElementsByClassName('mobile-nav');
+        mob[0].classList.remove("clk");
+        return a=1;
+    }
+});
