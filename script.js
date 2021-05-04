3a4afc0bbdbb3560f670fd1b923bc3bb970f861e
pinnintisagarSB <72218665+pinnintisagarSB@users.noreply.github.com>
2021-05-04 12:40:21 +0530

    Update 2.0
    Completely Fixed menu-bar.

--- script.js
+++ script.js
@@ -6,31 +6,19 @@ let a = 1;
 ico = document.getElementById('icon');
 ico.addEventListener('click', function () {
     if (a == 1) {
-        disablescroll();
         console.log("Open");
         let mob = document.getElementsByClassName('mobile-nav');
         mob[0].classList.add("clk");
-        var l= document.getElementsByClassName("logo");
+        var l = document.getElementsByClassName("logo");
         l[0].classList.add("cl");
         return a = 0;
     }
     else {
-        enablescroll();
         console.log("Close");
         let mob = document.getElementsByClassName('mobile-nav');
         mob[0].classList.remove("clk");
-        var l= document.getElementsByClassName("logo");
+        var l = document.getElementsByClassName("logo");
         l[0].classList.remove("cl");
         return a = 1;
     }
 });
-function disablescroll() {
-    scrollTop = document.documentElement.scrollTop;
-    scrollLeft = document.documentElement.scrollLeft;
-    window.onscroll = function () {
-        window.scrollTo(scrollTop, scrollLeft);
-    };
-};
-function enablescroll() {
-    window.onscroll = function () { };
-};
